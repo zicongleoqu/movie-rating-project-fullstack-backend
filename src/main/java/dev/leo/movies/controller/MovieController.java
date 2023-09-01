5706cd0d60e20e9095711aed41e4754e8618ce47
zicongleoqu <leoqu9577@gmail.com>
2023-08-31 20:48:12 -0400

    Set up Redis using Spring Data Redis

--- src/main/java/dev/leo/movies/controller/MovieController.java
+++ src/main/java/dev/leo/movies/controller/MovieController.java
@@ -6,6 +6,9 @@ import dev.leo.movies.repository.MovieRepository;
 import dev.leo.movies.service.MovieService;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -37,11 +40,13 @@ public class MovieController {
     }
 
     @GetMapping("/{imdbId}")
+    @Cacheable(value = "movie", key = "#imdbId")
     public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String imdbId){
         return new ResponseEntity<Optional<Movie>>(service.findByImdbId(imdbId), HttpStatus.OK);
     }
 
     @GetMapping(path="/{movieId}/average")
+    @Cacheable(value = "movieAverage", key = "movieId")
     public Map<String, Double> getAverage(@PathVariable(value = "movieId") ObjectId movieId) {
         verifyMovie(movieId);
         return Map.of("Movie average", movieRepository.findById(movieId).get().getReviews().stream()
@@ -58,6 +63,7 @@ public class MovieController {
 
     //Updating movie attributes (ImdbId and reviews stays unchanged)
     @PutMapping(path="/{id}")
+    @CachePut(value = "movie", key = "#id")
     public Movie updateMovieWithPut(@PathVariable(value="id") ObjectId id,
                                @RequestBody @Validated Movie movie) {
         verifyMovie(id);
@@ -72,6 +78,7 @@ public class MovieController {
     }
 
     @DeleteMapping(path= "/{movieId}")
+    @CacheEvict(value = "movie", allEntries = false, key = "#movieId")
     public void deleteMovie(@PathVariable(value="movieId") ObjectId movieId) {
         verifyMovie(movieId);
         movieRepository.deleteById(movieId);
